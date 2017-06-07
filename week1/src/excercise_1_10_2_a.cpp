740746405bd8789ddd50fe7cc93affec601beb67
René Reimann <rene.reimann@rwth-aachen.de>
2017-06-07 17:08:38 +0200

    made version with smart pointers, checked for mem-leaks and fixed them

--- week1/src/excercise_1_10_2_a.cpp
+++ week1/src/excercise_1_10_2_a.cpp
@@ -1,3 +1,8 @@
+// Summer of C++ 
+// Homework Week 1 
+// Excercise 1.10.2 part 1 from "Discovering Modern C++: An Intensive Course for Scientists, Engineers, and Programmers (C++ In-Depth)" by Peter Gottschling
+// Solution by: Rene Reimann
+
 /*
  * 1. Write the following declarations:
  *      pointer to a character
@@ -9,40 +14,36 @@
  *      pointer to an integer constant
  *      constant pointer to an integer
  *    Initialize all these objects.
- * 2. Make a small program that creates arrays on the stack (fixed-size 
- *    arrays) and arrays on the heap (using allocation). Use valgrind to
- *    check what happens when you do not delete them correctly.
  */
 
 #include <iostream>
 #include <memory>
 
 void define_simple_pointer(){
+    
     // pointer to a character
     //char * p1 = new char('a');
     char * p1;
-    p1 = new char('a');
+    p1 = new char('a');                                                 
     std::cout << p1 << " " << *p1 << std::endl;
-    p1 = new char('b');
+    //p1 = new char('b');                                               // leads to mem-leak, L.26 not deleted
     std::cout << p1 << " " << *p1 << std::endl;
-    *p1 = 'c';
+    //*p1 = 'c';                                                        // leads to mem-leak, L.28 not deleted
     std::cout << p1 << " " << *p1 << std::endl;
     
     // array of 10 integers
     int a1[10] = {1,2,3,4,5,6,7,8,9,0};
-    
     std::cout << a1 << " " << *a1 << std::endl;
     for (unsigned i=0; i<10; ++i){ std::cout << "   " << a1[i] << std::endl;}
     
     // pointer to an array of 10 integers
-    
-    int * p2 = new int[10];                     // can it be directly initialized?
-    for (unsigned i=0; i<10; ++i){ p2[i]=i*2;}  // initiallizing
+    int * p2 = new int[10];                                             // can it be directly initialized?
+    for (unsigned i=0; i<10; ++i){ p2[i]=i*2;}  
     std::cout << p2 << " " << *p2 << std::endl;
     for (unsigned i=0; i<10; ++i){ std::cout << "   " << p2[i] << std::endl;}
     
     // pointer to an array of character strings
-    std::string * p3 = new std::string[10];
+    std::string * p3 = new std::string[10];                             
     for (unsigned i=0; i<10; ++i){ p3[i]="Hello"+std::to_string(i);}
     std::cout << p3 << " " << *p3 << std::endl;
     for (unsigned i=0; i<10; ++i){ std::cout << "   " << p3[i] << std::endl;}
@@ -59,11 +60,11 @@ void define_simple_pointer(){
     // pointer to an integer constant
     const int * p5 = new const int(14);
     std::cout << p5 << " "<< *p5 << std::endl;
-    p5 = new const int(3);
-    std::cout << p5 << " "<< *p5 << std::endl;
+    // p5 = new const int(3);                                           // this leads to a memory leak because 
+    // std::cout << p5 << " "<< *p5 << std::endl;                       // the const int created in L 62 is not deleted
     
     // constant pointer to an integer
-    int * const p6 = new int(22);
+    int * const p6 = new int(22);                                       
     std::cout << p6 << " "<< *p6 << std::endl;
     //p6 = new int(3); // not allowed
     std::cout << p6 << " "<< *p6 << std::endl;
@@ -71,8 +72,13 @@ void define_simple_pointer(){
     std::cout << p6 << " "<< *p6 << std::endl;
     
     // Initialize all these objects.
-    
-    delete p1, p2, p3, p4, p5, p6;
+
+    delete p1;    
+    delete [] p2;
+    delete [] p3;
+    delete *p4, p4; // we must first delete the pointer on the char before we delete the pointer to the pointer
+    delete p5;
+    delete p6;
 }
 
 void define_smart_pointer(){
@@ -81,66 +87,44 @@ void define_smart_pointer(){
     std::cout << "P1: " << p1 << " *P1: " << *p1 << std::endl;
     
     // array of 10 integers
+    int a1[] = {1,2,3,4,5,6,7,8,9,0};
+    std::cout << a1 << " " << *a1 << std::endl;
+    for (unsigned i=0; i<10; ++i){ std::cout << "   " << a1[i] << std::endl;}
+    
     // pointer to an array of 10 integers
-    // pointer to an array of character strings
-    // pointer to pointer to a character
-    // interger constant
-    // pointer to an integer constant
-    // constant pointer to an integer
-}
-
-int main(){
-    define_simple_pointer();
-    //define_smart_pointer();
-    
-    /*
-    
-    //std::shared_ptr<char> p1 = std::make_shared<char>();
-    //*p1 = 'a';
-    std::cout << "P1: " << " *P1: " << p1 << std::endl;
-        
-    // array of 10 integers
-    int a1[] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    std::cout << "a1: " << a1 << std::endl;
-    for (unsigned i=0; i<10; ++i){ std::cout << "    a1[" << i << "]: " << a1[i] << std::endl;}
-
-    // pointer to an array of 10 integers    
-    std::unique_ptr<int[]> p2{new int[10]}; // how to do with shared_ptr // why can't initialize with  {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-    for (unsigned i=0; i<10; ++i){p2[i] = i+2;}
-    std::cout << "P2.get(): " << p2.get() << std::endl;
-    for (unsigned i=0; i<10; ++i){ std::cout << "    P2.get()[" << i << "]: " << p2.get()[i] << std::endl;}
+    std::unique_ptr<int[]> p2{new int[10]};
+    for (unsigned i=0; i<10; ++i){ p2[i] = 2*i; }
+    for (unsigned i=0; i<10; ++i){ std::cout << "   " << p2[i] << std::endl;}
     
     // pointer to an array of character strings
-    std::unique_ptr<std::string[]> p3{new std::string[10]}; // how to do with shared_ptr 
+    std::unique_ptr<std::string[]> p3{new std::string[10]};  
     for (unsigned i=0; i<10; ++i){ p3[i] = ('a'+i);}
-    std::cout << "*P3: " << p3.get() << std::endl;
     for (unsigned i=0; i<10; ++i){ std::cout << "    P3[" << i << "]: " << p3[i] << std::endl;}
     
     // pointer to pointer to a character
-    std::shared_ptr<std::shared_ptr<char>> p4 = std::make_shared<std::shared_ptr<char>>(); //{new char}; //
-    **p4 = 'b';
-    
-    std::cout << "P4: " << p4 << " *P4: " << *p4  << std::endl; // << " *P4: " << *p4 << " P4->get(): " << p4->get()
-    **p4 = 'b';
+    std::shared_ptr<char> p_temp = std::make_shared<char>('g');
+    std::shared_ptr<std::shared_ptr<char>> p4 = std::make_shared<std::shared_ptr<char>>(p_temp);
+    std::cout << "P4: " << p4 << " *P4: " << *p4 << " **P4: " << **p4  << std::endl;
     
     // interger constant
     const int c1 = 15;
     std::cout << "c1: " << c1 << std::endl;
-    */
-    //std::shared_ptr<const int> p5{new const int 15};// = std::make_shared<const int>();
-    //std::cout << "p5: " << p5 << "p5*: " << p5* << std::endl;
-    /*
-
     
-     
     // pointer to an integer constant
-    int *pointer_to_an_integer_constant = const 15 ; 
-
+    std::shared_ptr<const int> p5 = std::make_shared<const int>(15);
+    std::cout << "*p5: " << *p5 << std::endl;
+    
     // constant pointer to an integer
-    const int *constant_pointer_to_an_integer = 15;
+    const std::shared_ptr<int> p6 = std::make_shared<int>(16);
+    std::cout << "*p6: " << *p6 << std::endl;
+    *p6 += 1;
+    std::cout << "*p6: " << *p6 << std::endl;
+    // p6 = std::make_shared<int>(18); // not allowed
+}
 
+int main(){
+    define_simple_pointer();
+    define_smart_pointer();
 
-
-    */
     return 0;
 }
