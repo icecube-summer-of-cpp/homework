e964081ab5547bdd4dec192f96681276fdb607b4
kkrings <kai.krings88@gmx.net>
2017-06-08 16:00:48 +0200

    Add my solutions for the first week's homework.

---
+++ kkrings/chapter01/exercise_1.10.3.cpp
@@ -0,0 +1,37 @@
+/**
+ * Solution to exercise 1.10.3
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main()
+{
+    unsigned int nrow = 0, ncol = 0, nonzero = 0;
+
+    std::ifstream infile("matrix.dat");
+    if (infile.good())
+    {
+        std::string line;
+        while (std::getline(infile, line))
+        {
+            if (line[0] != '%')
+            {
+                std::istringstream stream(line);
+                stream >> nrow >> ncol >> nonzero;
+                break;
+            }
+        }
+    }
+    else
+    {
+        std::cerr << "Could not open matrix.dat." << std::endl;
+    }
+
+    std::cout << "Number of rows: " << nrow << ", "
+              << "number of columns: " << ncol << ", "
+              << "number of non-zero entries" << nonzero
+              << std::endl;
+}
