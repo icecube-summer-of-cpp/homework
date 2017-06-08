3d0aabe1c763376bea72a2d7b982ec26cdb15ad9
christophra <christoph@raab.io>
2017-06-08 13:54:03 +0200

    Exercise 1.10.1: forgot file

--- week-1/1.10.1/ask_age.cxx
+++ week-1/1.10.1/ask_age.cxx
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+
 
 int main ()
 {
@@ -6,13 +8,17 @@ int main ()
     std::cout << "What is your age?" << std::endl;
     std::cin >> age;
     
+    std::ofstream age_file("age.txt");
+    
     if (age < 0)
     {
         std::cout << "I don't believe you!" << std::endl;
+        age_file << age << std::endl;
     }
     else
     {
         std::cout << "You are " << age << " years old." <<    std::endl;
+        age_file << age << std::endl;
     }
     
     return 0;
