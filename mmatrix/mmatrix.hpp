f83a6c5d1b20bf583c04d21c567f00aa4605f410
Federica Bradascio <federica.bradascio@gmail.com>
2017-06-08 16:01:18 +0200

    Add files via upload

---
+++ mmatrix/mmatrix.hpp
@@ -0,0 +1,14 @@
+/** 
+ *  Summer of C++
+ *  Week 1, Exercise 1.10.3
+ *  @author: Federica Bradascio
+ *
+ * Read the header of a  Matrix Market file and print
+ * the number of rows, the number of columns and the
+ * number of non-zero elements on the spare matrix.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
