e33a18f38b7c1255f42ebb656d48cf255b0fa4b5
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-06-08 09:52:44 -0400

    Modified for slack

--- hw01/10.3/10.3.cpp
+++ hw01/10.3/10.3.cpp
@@ -3,37 +3,27 @@
 #include <string>
 #include <sstream>
 
-/*
-// Doesn't work, can't get the row, col, nonzero numbers extracted
-// using that sstream thing
-*/
-
 int main(int argc, char const *argv[])
 {
     std::ifstream infile("market_matrix.dat");
 
     std::string line;
-    while (std::getline(infile, line))
+    while (std::getline(infile, line))  // res line by line
     {
-        std::istringstream instream(line);
-        // std::cout << line << std::endl;
-
-        if (line[0] == '%') {
+        std::istringstream instream(line);  // make stream from string
+        if (line[0] == '%') {  // skip comments
             continue;
-            // std::cout << "-> Comment line" << std::endl;
         }
-        else {
+        else {  // extract data from first valid line with 3 cols
+            int rows, cols, nonzeros;
             std::cout << line << std::endl;
-
-            int rows, cols, sparse_nonzeros;
-            instream >> rows >> cols >> sparse_nonzeros;
-            std::cout << "Rows = " << rows
-                      << " Cols = " << cols
-                      << " nonzeros = " << sparse_nonzeros
-                      << std::endl;
+            instream >>  std::skipws >> rows >> cols >> nonzeros;
+            std::cout << "Rows     = " << rows << std::endl
+                      << "Cols     = " << cols << std::endl
+                      << "nonzeros = " << nonzeros << std::endl;
             break;
-        } // Close after first valid row
+        } // just close after first valid row
     }
-
+    infile.close();
     return 0;
 }
