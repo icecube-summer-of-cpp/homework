0ce2683efd72f30e9665b7a1c747fd290390a7e2
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-06-13 14:26:45 -0500

    Improved printing function

--- hw2/p_2_8_1.cpp
+++ hw2/p_2_8_1.cpp
@@ -15,7 +15,16 @@ class polynomial
 {
     public:
         // Default constructor
-        polynomial(std::vector<double> coefficients={0}) : coeff(coefficients) {}
+        polynomial(std::vector<double> coefficients={0}) : coeff(coefficients)
+        {
+            // Catch if all elements are zero
+            if (this->first_nonzero()<0) {
+                coeff = {0};
+            }
+        }
+
+        // Constructor when double is passed
+        polynomial(double coefficient) : coeff({coefficient}) {}
 
         // Destructor not necessary, vector of coefficients manages itself
         // ~polynomial() {}
@@ -31,6 +40,16 @@ class polynomial
     
     private:
         std::vector<double> coeff;
+
+        int first_nonzero() const
+        {
+            for (int i=0; i<coeff.size(); ++i) {
+                if (coeff[i]!=0) {
+                    return i;
+                }
+            }
+            return -1;
+        }
 };
 
 
@@ -43,14 +62,17 @@ std::ostream& operator<<(std::ostream& os, const polynomial& p)
     }
 
     // Longer output for longer polynomials
-    for (int i=0; i<p.coeff.size(); ++i) {
+    int start = p.first_nonzero();
+    for (int i=start; i<p.coeff.size(); ++i) {
         if (p.coeff[i]==0) {
             continue;
         }
-        if (i>0) {
+        else if (i>start) {
             os << " + ";
         }
-        os << p.coeff[i];
+        if (p.coeff[i]!=1 || i==0) {
+            os << p.coeff[i];
+        }
         if (i>0) {
             os << "x";
         }
@@ -65,10 +87,18 @@ std::ostream& operator<<(std::ostream& os, const polynomial& p)
 
 int main()
 {
-    polynomial a({1});
+    polynomial a({1,2});
     polynomial b = {{2,3}};
-    polynomial d = {{4,0,5,6}};
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << d << std::endl;
+    polynomial c = 2.5;
+    polynomial d = {{0,0,4,0,0,5,0,6}};
+    polynomial e = {{0,1,0,0,0}};
+    polynomial z = 0;
+    polynomial z2 = {{0,0}};
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "c = " << c << std::endl;
+    std::cout << "d = " << d << std::endl;
+    std::cout << "e = " << e << std::endl;
+    std::cout << "z = " << z << std::endl;
+    std::cout << "z2 = " << z2 << std::endl;
 }
