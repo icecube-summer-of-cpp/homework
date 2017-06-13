1a447de210f8b6073b04f4e75f6d28f79065daf7
heiwais25 <heiwais25@gmail.com>
2017-06-13 14:58:37 +0900

    modify source code to newer version

--- week1/exe_1.10.3/src_1.cc
+++ week1/exe_1.10.3/src_1.cc
@@ -10,26 +10,45 @@
 #include <string>
 using namespace std;
 
+void read_matrix_file(const char * fname) noexcept;
+
+struct cannot_open_file{};
+
 int main(void)
 {
 	char check_line;
 	int rows_matrix = 0, columns_matrix = 0, num_non_zero_matrix = 0;
 	string line;
-	ifstream in_matrix_file;
-	in_matrix_file.open("example_matrix_market");
-
-	while(getline(in_matrix_file, line))
-	{
-		if(line.at(0) != '%')
-		{
-			stringstream linestream(line);
-			linestream >> rows_matrix;
-			linestream >> columns_matrix;
-			linestream >> num_non_zero_matrix;
-			break;
-		}
+	
+	try{
+		read_matrix_file("market");
+	}catch(cannot_open_file & e){
+		cout << "Cannot open the file" << endl;
 	}
-	cout << "Number of row : " << rows_matrix << "\tNumber of column : " << columns_matrix << 
-		"\tNumber of non zero : " << num_non_zero_matrix << endl;
+	// ifstream in_file("example_matrix_market");
+	// in_file.open("example_matrix_market");
+
+	// while(getline(in_file, line))
+	// {
+	// 	if(line.at(0) != '%')
+	// 	{
+	// 		stringstream linestream(line);
+	// 		linestream >> rows_matrix;
+	// 		linestream >> columns_matrix;
+	// 		linestream >> num_non_zero_matrix;
+	// 		break;
+	// 	}
+	// }
+	// cout << "Number of row : " << rows_matrix << "\tNumber of column : " << columns_matrix << 
+	// 	"\tNumber of non zero : " << num_non_zero_matrix << endl;
 	return 0;
+}
+
+void read_matrix_file(const char * fname) noexcept
+{
+	fstream in_file(fname);
+	cout << in_file.is_open() << endl;
+
+	if(!in_file.is_open())
+		throw cannot_open_file{};
 }
--- week1/quiz/src/src_1.cc
+++ week1/quiz/src/src_1.cc
@@ -3,32 +3,50 @@
 // For a sparse matrix, the data is stored in three columns. The first column is the row number, 
 // the second column the column number, and the third column the numerical value. When the value type of the matrix is complex, 
 // a fourth column is added for the imaginary part.
-
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+ #include <fstream>
+ #include <sstream>
+ #include <memory>
+ #include <vector>
+ #include <complex>
 using namespace std;
+
+inline void enable_stream_exception(std::ios& io){
+	io.exceptions(std::ios::badbit | std::ios::failbit);
+}
+
+enum process_situation{
+	FAIL,
+	SUCCESS,
+};
 
 int main(int argc, char* argv[])
 {
-	char * file_name;
-	if(argc < 2)
-	{
+	string file_name;
+	int number;
+	ifstream in_file;
+
+	if(argc < 2)	{
 		cout << "Please input file name : ";
 		cin >> file_name;
 	}
-	else
-	{
+	else {
 		file_name = argv[1];
 	}
-	char check_line;
-	int rows_matrix = 0, columns_matrix = 0, num_non_zero_matrix = 0;
+
+	fstream check_file(file_name.c_str());
+	enable_stream_exception(in_file);
+	try{
+		in_file.open(file_name.c_str());
+	} catch(std::ios_base::failure &fail){
+		cout << "You wrote wrong file name" << endl;
+		return FAIL;
+	} 
+
 	string line;
+	int rows_matrix = 0, columns_matrix = 0, num_non_zero_matrix = 0;
 	 
-	ifstream in_matrix_file;
-	in_matrix_file.open(file_name);
-	while(getline(in_matrix_file, line))
+	while(getline(in_file, line))
 	{
 		if(line.at(0) != '%')
 		{
