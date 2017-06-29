848f8f4aca5c767f170b75773dc316b0e65c1ecd
René Reimann <renereimann@github.com>
2017-06-30 01:36:14 +0200

    Solution to quiz_week_3/ unique_id, can be created, compared and copied. Works as expected. The only thing I still do not get is how to interpret, the cheap creation vs expensive creation

---
+++ quiz_week_3/src/unique_id.cpp
@@ -0,0 +1,26 @@
+// Summer of C++ 
+// Quiz Week 3 
+// Solution by: Rene Reimann
+
+/*
+ * Write a class that serves as a unique identifier for other objects. 
+ * I should be able to use this identifier as a class member to use for comparisons.
+ * 
+ * The class should compare equal to copies of itself, but not to objects 
+ * created at the same time, on the same machine, in different processes.
+ * 
+ * It should not be computationally expensive to create subsequent IDs 
+ * in one process. (Hint: You can use two private members: one that is 
+ * expensive to compute and one that's cheap to compute. 
+ * Unique ID objects compare equal if the two members compare equal.)
+ * 
+ * Follow the "Rule of Six."
+ * 
+ * Below is an example of a typical use case
+ */
+
+#include "unique_id.hpp"
+
+// has to be in the source file, not in the header, because otherwise 
+// initialization may happen multiple times, leading to linking errors
+unsigned unique_id::count_ = 0;
