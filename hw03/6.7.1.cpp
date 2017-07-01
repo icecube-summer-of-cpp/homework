f286daebf07f84ad69fc8cc4610c0bfa9022ab79
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-07-01 11:21:09 +0200

    6.7.1.cpp::Example from the book, to be modified for hw03

--- hw03/6.7.1.cpp
+++ hw03/6.7.1.cpp
@@ -5,17 +5,19 @@
 class person
 {
 public:
-    explicit person(const std::string& new_name) : name(new_name) {};
+    explicit person(const std::string& name) : name(name) {};
+    // Explicit rule of six
+    person () = default;
     ~person() = default;
-    person(const person& copy_person) = delete;
-    person(person&& move_person) = delete;
-    person& operator=(const person& copy_person) = delete;
-    person& operator=(person& move_person) = delete;
+    person(const person& copy_person) = default;
+    person(person&& move_person) = default;
+    person& operator=(const person& copy_person) = default;
+    person& operator=(person& move_person) = default;
 
-    void set_name(const std::string& new_name) { name = new_name; }
-    std::string get_name() { return name; };
+    void set_name(const std::string& n) { name = n; }
+    std::string get_name() const { return name; };
 
-    void all_info() const {
+    virtual void all_info() const {
         std::cout << "[person] My name is " << name << std::endl;
     }
 
@@ -24,9 +26,57 @@ private:
 };
 
 
+class student : public person
+{
+public:
+    student(const std::string& name, const std::string& passed) :
+        person(name), passed(passed) {}
+
+    virtual void all_info() const override {
+        std::cout << "[student] My name is " << get_name() << std::endl;
+        std::cout << "  I passed the following grades: " << passed << std::endl;
+    }
+
+private:
+    std::string passed;
+};
+
+
+class mathematician : public person
+{
+public:
+    mathematician(const std::string& name, const std::string& proved) :
+        person(name), proved(proved) {};
+
+    virtual void all_info() const override {
+        person::all_info();
+        std::cout << "  I proved: " << proved << std::endl;
+    };
+
+private:
+    std::string proved;
+};
+
+
+class math_student : public student, public mathematician
+{
+public:
+    math_student(const std::string& name,
+                 const std::string& passed,
+                 const std::string& proved) :
+                 student(name, passed), mathematician(name, proved) {};
+
+                 virtual void all_info() const override {
+                    student::all_info();
+                    mathematician::all_info();
+                 };
+};
+
 
 int main(int argc, char const *argv[])
 {
+    math_student bob("Robert Robson", "Algebra", "Fermat's Last Theorem");
+    bob.all_info();
 
     return 0;
 }
