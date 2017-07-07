3c04bbd2c3750755761412e36707e41e6e97dcd9
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-07-07 13:27:14 -0500

    Added basic functionality to vectors

--- hw6/vectors.cpp
+++ hw6/vectors.cpp
@@ -11,13 +11,8 @@
 
 #include "vectors.hpp"
 
-//Cartesian vector length function
-double CartesianVector::length()
-{
-    return sqrt(x_*x_ + y_*y_ + z_*z_);
-}
 
-// Unit vector constructor
+// Unit vector constructors
 UnitVector::UnitVector(double x, double y, double z)
 {
     x_ = x;
@@ -27,11 +22,222 @@ UnitVector::UnitVector(double x, double y, double z)
 }
 
 
+// Unit vector bracket overload
+const double& UnitVector::operator[](int i) const
+{
+    if      (i==0) {return x_;}
+    else if (i==1) {return y_;}
+    else if (i==2) {return z_;}
+    // TODO: Add error in else statement
+}
+
+
 // Unit vector normalization function
 void UnitVector::normalize()
 {
-    double len = length();
-    x_ /= len;
-    y_ /= len;
-    z_ /= len;
+    double len = mag(*this);
+    if (len==0) {
+        x_ = 0;
+        y_ = 0;
+        z_ = 1;
+    }
+    else {
+        x_ /= len;
+        y_ /= len;
+        z_ /= len;
+    }
 }
+
+
+
+// Cartesian vector bracket overloads
+double& CartesianVector::operator[](int i)
+{
+    if      (i==0) {return x_;}
+    else if (i==1) {return y_;}
+    else if (i==2) {return z_;}
+    // TODO: Add error in else statement
+}
+
+const double& CartesianVector::operator[](int i) const
+{
+    if      (i==0) {return x_;}
+    else if (i==1) {return y_;}
+    else if (i==2) {return z_;}
+    // TODO: Add error in else statement
+}
+
+
+// Cartesian vector arithmetic operations
+CartesianVector& CartesianVector::operator+=(const CartesianVector& rhs)
+{
+    x_ += rhs.x();
+    y_ += rhs.y();
+    z_ += rhs.z();
+    return *this;
+}
+
+CartesianVector& CartesianVector::operator-=(const CartesianVector& rhs)
+{
+    x_ -= rhs.x();
+    y_ -= rhs.y();
+    z_ -= rhs.z();
+    return *this;
+}
+
+CartesianVector& CartesianVector::operator*=(const double rhs)
+{
+    x_ *= rhs;
+    y_ *= rhs;
+    z_ *= rhs;
+    return *this;
+}
+
+
+CartesianVector operator+(const CartesianVector& lhs, const CartesianVector& rhs)
+{
+    CartesianVector temp(lhs);
+    return temp += rhs;
+}
+
+CartesianVector operator-(const CartesianVector& lhs, const CartesianVector& rhs)
+{
+    CartesianVector temp(lhs);
+    return temp -= rhs;
+}
+
+CartesianVector operator*(const CartesianVector& lhs, const double rhs)
+{
+    CartesianVector temp(lhs);
+    return temp *= rhs;
+}
+
+CartesianVector operator*(const double lhs, const CartesianVector& rhs)
+{
+    CartesianVector temp(rhs);
+    return temp *= lhs;
+}
+
+
+
+// Four-vector bracket overloads
+double& FourVector::operator[](int i)
+{
+    if      (i==0) {return w_;}
+    else if (i==1) {return x_;}
+    else if (i==2) {return y_;}
+    else if (i==3) {return z_;}
+    // TODO: Add error in else statement
+}
+
+const double& FourVector::operator[](int i) const
+{
+    if      (i==0) {return w_;}
+    else if (i==1) {return x_;}
+    else if (i==2) {return y_;}
+    else if (i==3) {return z_;}
+    // TODO: Add error in else statement
+}
+
+
+// Four-vector arithmetic operations
+FourVector& FourVector::operator+=(const FourVector& rhs)
+{
+    w_ += rhs.w();
+    x_ += rhs.x();
+    y_ += rhs.y();
+    z_ += rhs.z();
+    return *this;
+}
+
+FourVector& FourVector::operator-=(const FourVector& rhs)
+{
+    w_ -= rhs.w();
+    x_ -= rhs.x();
+    y_ -= rhs.y();
+    z_ -= rhs.z();
+    return *this;
+}
+
+FourVector& FourVector::operator*=(const double rhs)
+{
+    w_ *= rhs;
+    x_ *= rhs;
+    y_ *= rhs;
+    z_ *= rhs;
+    return *this;
+}
+
+
+FourVector operator+(const FourVector& lhs, const FourVector& rhs)
+{
+    FourVector temp(lhs);
+    return temp += rhs;
+}
+
+FourVector operator-(const FourVector& lhs, const FourVector& rhs)
+{
+    FourVector temp(lhs);
+    return temp -= rhs;
+}
+
+FourVector operator*(const FourVector& lhs, const double rhs)
+{
+    FourVector temp(lhs);
+    return temp *= rhs;
+}
+
+FourVector operator*(const double lhs, const FourVector& rhs)
+{
+    FourVector temp(rhs);
+    return temp *= lhs;
+}
+
+
+// Vector print functions
+std::ostream& operator<<(std::ostream& os, const UnitVector& v)
+{
+    return os << "(" << v.x() << "," << v.y() << "," << v.z() << ")";
+}
+
+std::ostream& operator<<(std::ostream& os, const CartesianVector& v)
+{
+    return os << "(" << v.x() << "," << v.y() << "," << v.z() << ")";
+}
+
+std::ostream& operator<<(std::ostream& os, const FourVector& v)
+{
+    return os << "(" << v.w() << "," << v.x() << "," << v.y() << "," << v.z() << ")";
+}
+
+
+// Vector magnitude functions
+double mag(const CartesianVector& v)
+{
+    return sqrt(dot(v,v));
+}
+
+double mag(const FourVector& v)
+{
+    return sqrt(dot(v,v));
+}
+
+
+// Vector product functions
+double dot(const CartesianVector& u, const CartesianVector& v)
+{
+    return u.x()*v.x() + u.y()*v.y() + u.z()*v.z();
+}
+
+double dot(const FourVector& u, const FourVector& v)
+{
+   return u.w()*v.w() - u.x()*v.x() - u.y()*v.y() - u.z()*v.z();
+}
+
+CartesianVector cross(const CartesianVector& u, const CartesianVector& v)
+{
+    return CartesianVector(u.y()*v.z() - u.z()*v.y(),
+                           u.z()*v.x() - u.x()*v.z(),
+                           u.x()*v.y() - u.y()*v.x());
+}
+
--- hw6/vectors.hpp
+++ hw6/vectors.hpp
@@ -1,6 +1,6 @@
 /* Module for vectors designed to work for particle module
  * Includes:
- *   CartesianVector class (3D vector with x, y, z)
+ *   CartesianVector struct (3D vector with x, y, z)
  *   UnitVector class (3D vector normalized to length 1)
  *   FourVector class (4D vector with w, x, y, z - designed for four-momenta)
  *   Functions for manipulating vectors
@@ -14,31 +14,50 @@
 #define __VECTORS_HPP_INCLUDED__
 
 #include <math.h>
+#include <iostream>
+
+
+class UnitVector
+{
+    public:
+        // Constructors
+        UnitVector(double x=1, double y=0, double z=0);
+
+        // Getter functions
+        const double x() const {return x_;}
+        const double y() const {return y_;}
+        const double z() const {return z_;}
+        const double& operator[](int i) const;
+
+    private:
+        double x_, y_, z_;
+        void normalize();
+};
 
 
 class CartesianVector
 {
     public:
-        // Default constructor
+        // Constructors
         CartesianVector(double x=0, double y=0, double z=0)
         : x_(x), y_(y), z_(z) {};
+        CartesianVector(UnitVector& v)
+        : x_(v.x()), y_(v.y()), z_(v.z()) {};
 
-        // Length function
-        double length();
+        // Getter functions
+        const double x() const {return x_;}
+        const double y() const {return y_;}
+        const double z() const {return z_;}
+        double& operator[](int i);
+        const double& operator[](int i) const;
 
-    protected:
-        double x_, y_, z_;
-};
-
-
-class UnitVector : public CartesianVector
-{
-    public:
-        // Default constructor
-        UnitVector(double x=1, double y=0, double z=0);
+        // Arithmetic functions
+        CartesianVector& operator+=(const CartesianVector& rhs);
+        CartesianVector& operator-=(const CartesianVector& rhs);
+        CartesianVector& operator*=(const double rhs);
 
     private:
-        void normalize();
+        double x_, y_, z_;
 };
 
 
@@ -49,9 +68,51 @@ class FourVector
         FourVector(double w=0, double x=0, double y=0, double z=0)
         : w_(w), x_(x), y_(y), z_(z) {};
 
+        // Getter functions
+        const double w() const {return w_;}
+        const double x() const {return x_;}
+        const double y() const {return y_;}
+        const double z() const {return z_;}
+        double& operator[](int i);
+        const double& operator[](int i) const;
+
+        // Arithmetic functions
+        FourVector& operator+=(const FourVector& rhs);
+        FourVector& operator-=(const FourVector& rhs);
+        FourVector& operator*=(const double rhs);
+
     private:
         double w_, x_, y_, z_;
 };
+
+
+// Vector print functions
+std::ostream& operator<<(std::ostream& os, const UnitVector& v);
+std::ostream& operator<<(std::ostream& os, const CartesianVector& v);
+std::ostream& operator<<(std::ostream& os, const FourVector& v);
+
+
+// Vector magnitude functions
+double mag(const CartesianVector& v);
+double mag(const FourVector& v);
+
+
+// Vector product functions
+double dot(const CartesianVector& u, const CartesianVector& v);
+double dot(const FourVector& u, const FourVector& v);
+CartesianVector cross(const CartesianVector& u, const CartesianVector& v);
+
+
+// Arithmetic functions
+CartesianVector operator+(const CartesianVector& lhs, const CartesianVector& rhs);
+CartesianVector operator-(const CartesianVector& lhs, const CartesianVector& rhs);
+CartesianVector operator*(const CartesianVector& lhs, const double rhs);
+CartesianVector operator*(const double lhs, const CartesianVector& rhs);
+
+FourVector operator+(const FourVector& lhs, const FourVector& rhs);
+FourVector operator-(const FourVector& lhs, const FourVector& rhs);
+FourVector operator*(const FourVector& lhs, const double rhs);
+FourVector operator*(const double lhs, const FourVector& rhs);
 
 
 #endif
