ddf048090dba072c6fb229d81b39212531104281
René Reimann <renereimann@github.com>
2017-07-18 14:46:51 +0200

    clean up in test.cpp

--- week6/src/test.cpp
+++ week6/src/test.cpp
@@ -22,24 +22,13 @@ int main (int argc, char **argv)
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
     
-    //z = {0, 1, 2};
-    //std::cout << z << std::endl;
-    
     /*
     particle p1;
     particle p2;
-   
     p1.set_energy(1000);
     p2.set_energy(1000);
-   
     // p1 and p2 are not the same particle even if their properties are different.
     assert(p1 != p2);
-   
-    unique_id test_id(p1.id()); // need to be able to copy them and compare later
-   
     std::vector<particle> particle_list = {p1, p2};
-   
-    // i should be able to put them in containers and compare as expected
-    assert( test_id == particle_list.front().id() );
     */
 }
