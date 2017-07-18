260d1e0a06c3e1d575cbc0fa542711c49022a704
René Reimann <renereimann@github.com>
2017-07-18 17:11:09 +0200

    clean up old stuff and implemented first test and ostream free function for particle

--- week6/src/particle.cpp
+++ week6/src/particle.cpp
@@ -2,23 +2,22 @@
 
 // test particle
 
-/* constructor, assignment, move, destructor, comparison, getter, setters, tests, comments, printing, functionallity
- * 
- * PARTICLE
- * --------
- * + position 
- * + time
- * + energy
- * + momentum
- * + unique_id
- * + quantum numbers
- * - direction
- * - mass
- * - type
- * - charge
- * 
- */
+// constructor
+
+// print 
+std::ostream& operator <<(std::ostream& os, const particle& p){
+    return os << "type: " << charge(p.quantum_numb_) << " Time/Space: " << p.time_space_ 
+              << " Energy/Momentum: " << p.energy_momentum_ 
+              << " identifier: " << p.id_;
+}
  
-enum class fit_status {ok, failed, partial};
-enum class sim_particle {primary, cascade_segment, dark};
-enum class reco_particle {inice, };
+// tests
+
+TEST(ParticleTest, constructor) {
+    particle a;
+    particle b(a);
+    particle c(particle_type::muon);
+    EXPECT_EQ(a, b);
+    std::cout << a << std::endl;
+    std::cout << c << std::endl;
+}
