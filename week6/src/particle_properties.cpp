892323a1937aad4a63dab094082b85ebc39cdb6c
René Reimann <renereimann@github.com>
2017-07-18 17:39:00 +0200

    added name and mass properties for particle_type

--- week6/src/particle_properties.cpp
+++ week6/src/particle_properties.cpp
@@ -8,4 +8,24 @@ float charge(particle_type p_type){
         case particle_type::muon: return -1.;
         case particle_type::neutrino: return 0.;
     }
-}
+};
+
+float mass(particle_type p_type){
+    switch(p_type) {
+        case particle_type::undefined: return NAN;
+        case particle_type::proton: return 940.;
+        case particle_type::photon: return 0.;
+        case particle_type::muon: return 211.;
+        case particle_type::neutrino: return 0.;
+    }
+};
+
+std::string name(particle_type p_type){
+    switch(p_type) {
+        case particle_type::undefined: return std::string("UNDEFINED");
+        case particle_type::proton: return std::string("Proton");
+        case particle_type::photon: return std::string("Photon");
+        case particle_type::muon: return std::string("Muon");
+        case particle_type::neutrino: return std::string("Neutrino");
+    }
+};
--- week6/src/particle_properties.hpp
+++ week6/src/particle_properties.hpp
@@ -4,28 +4,39 @@
 /*
  * Particle properties
  * 
+ * We define enum classes for 
+ *      particle_type
+ *      fit_status
+ *      sim_particles
+ *      reco_shape
  * 
+ * We also define functions that give properties of the enums, 
+ * e.g. charge(particle_type)
  * 
  */
- 
+
+// includes 
 #include <cmath>
 #include <iostream>
+#include <sstream>
 
-// returns the integer of the enum 
+// first lets define the enums
+enum class particle_type { undefined, proton, photon, muon, neutrino };
+enum class fit_status {ok, failed, partial, warn};
+enum class sim_particle {primary, cascade_segment, dark};
+enum class reco_shape {inice};
+
+// parameters of a particle_type
+float charge(particle_type);
+float mass(particle_type);
+std::string name(particle_type); 
+
+// get the int of an enum 
 template <typename Enumeration>
 auto as_integer(Enumeration const value)
     -> typename std::underlying_type<Enumeration>::type
 {
     return static_cast<typename std::underlying_type<Enumeration>::type>(value);
 }
-
-enum class particle_type { undefined, proton, photon, muon, neutrino };
-enum class fit_status {ok, failed, partial};
-enum class sim_particle {primary, cascade_segment, dark};
-enum class reco_status {ok, failed, warn};
-enum class reco_shape {inice};
-
-// parameters of a particle_type
-float charge(particle_type p_type);
 
 #endif
