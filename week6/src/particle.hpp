81e0d4acd91bda617e147ed42c7f5486a59eeffd
René Reimann <renereimann@github.com>
2017-07-18 17:13:51 +0200

    first working particle, with constructor, assignment, printing atleast

--- week6/src/particle.hpp
+++ week6/src/particle.hpp
@@ -1,48 +1,60 @@
 #ifndef PARTICLE
 #define PARTICLE
 
-#include <cmath>            // for NAN
-#include "unique_id.hpp"
-
-/* constructor, assignment, move, destructor, comparison, getter, setters, tests, comments, printing, functionallity
+/* 
  * 
  * PARTICLE
  * --------
- * + time / position 
- * + energy / momentum 
- * + unique_id
- * + quantum numbers
- * - direction
- * - mass
- * - type
- * - charge
+ * A particle is a particle in particle physics sens. It has quantum 
+ * numbers, position at a time point and energy and momentum. 
+ * It has also a unique identifier.
  * 
  */
 
+#include <cmath>            // for NAN
+#include "unique_id.hpp"
+#include "four_vector.hpp"
+#include "particle_properties.hpp"
+#include "gtest/gtest.h"
+
 class particle{
     public:
-        // i should get a unique identifier with the default constructor
-        // of unique_id without having to do anything, so no need to include
-        // it in the initialization list
-        particle(): energy_(NAN) {};
+        // constructors
+        particle(): quantum_numb_(particle_type::undefined) {};
+        particle(particle_type p_type): quantum_numb_(p_type) {};
+        particle(const particle&) = default;
+        particle(particle&&) = default;
+        //particle(std::initializer_list<float> values);
        
-        // no setter for ID.  just a getter.
+        // assignments
+        particle& operator=(const particle&) = default;
+        particle& operator=(particle&&) = default;
+        //particle& operator=(std::initializer_list<float> values);
+        
+        // destructor
+        ~particle() = default;   
+
+        // comparisons
+        bool operator==(const particle& rhs) const { return (id_ == rhs.id()); }; // (energy_ == rhs.get_energy()) && 
+        bool operator!=(const particle& rhs) const { return !(*this == rhs); };
+       
+        // getter and setter
         unique_id id() const { return id_; }
-       
-        void set_energy(float energy){ energy_ = energy; }
-        float get_energy() const { return energy_; }
-       
-        bool operator==(const particle& rhs) const {
-            return (energy_ == rhs.get_energy()) && (id_ == rhs.id());
-        }; // neglect issues with float comparisons
-       
-        bool operator!=(const particle& rhs) const {
-            return !(*this == rhs);
-        }
- 
+        //void set_energy(float energy){ energy_ = energy; }
+        //float get_energy() const { return energy_; }
+   
+        // functionallity
+        
     private:
+        particle_type quantum_numb_; // mass, type, charge
+        four_vector time_space_;
+        four_vector energy_momentum_;
         unique_id id_;
-        float energy_;
+    
+    friend std::ostream& operator <<(std::ostream&, const particle&);
 };
+
+// printing
+std::ostream& operator <<(std::ostream&, const particle&);
 
 #endif
