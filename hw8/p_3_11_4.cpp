95cae1c834ee73bb5509c017d47fd89a4b8406e0
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-07-25 14:08:40 -0500

    Added Gottschling list and iterator structs

--- hw8/p_3_11_4.cpp
+++ hw8/p_3_11_4.cpp
@@ -8,7 +8,8 @@
  */
 
 
-#include <cassert>
+#include <algorithm>
+#include <exception>
 #include <iostream>
 #include <memory>
 
@@ -20,8 +21,25 @@ class vector
         int                   my_size;
         std::unique_ptr<T[]>  data;
 
-        void check_size(int that_size) const { assert(my_size == that_size); }
-        void check_index(int i) const { assert(i >= 0 && i < my_size); }
+        void check_size(int that_size) const
+        {
+            if (my_size != that_size)
+            {
+                throw std::domain_error("Vectors are not the same size");
+            }
+        }
+
+        void check_index(int i) const
+        {
+            if (i < 0)
+            {
+                throw std::domain_error("Underflow when accessing vector");
+            } 
+            else if (i >= my_size)
+            {
+                throw std::domain_error("Overflow when accessing vector");
+            }
+        }
 
     public:
         explicit vector(int size)
@@ -75,12 +93,76 @@ std::ostream& operator<<(std::ostream& os, const vector<T>& v)
   for (int i= 0; i < v.size(); ++i) os << v[i] << ',';
   os << ']';
   return os;
-}
+};
+
+
+
+template <typename T>
+struct list_entry
+{
+    list_entry(const T& value) : value(value), next(0) {}
+
+    T              value;
+    list_entry<T>* next;
+};
+
+template <typename T>
+struct list_iterator
+{
+    list_iterator(list_entry<T>* entry) : entry(entry) {}
+    bool operator!=(const list_iterator<T>& that) const 
+    { return entry != that.entry; }
+
+    list_iterator<T> operator++() 
+    { entry= entry->next; return *this; }
+
+    list_iterator<T> operator++(int) 
+    {
+        list_iterator<T> tmp(*this);
+        entry= entry->next;
+        return tmp;
+    }
+    T& operator*() { return entry->value; }
+    T operator*() const { return entry->value; }
+
+    private:
+        list_entry<T>* entry;
+};
+
+template <typename T>
+struct list
+{
+    list() : first(0), last(0) {}
+    ~list() { while (first) { list_entry<T> *tmp= first->next; delete first; first= tmp; } }
+    void append(const T& x) { last= (first ? last->next : first)= new list_entry<T>(x); }
+    list_iterator<T> begin() const { return list_iterator<T>(first); }
+    list_iterator<T> end() const { return list_iterator<T>(0); }
+
+    list_entry<T> *first, *last;
+};
+
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const list<T>& l)
+{
+    for (list_iterator<T> it=l.begin();  it != l.end(); ++it) os << *it << ',';
+    return os;
+};
 
 
 
 int main()
 {
-    vector<int> a;
+    vector<int> a(3);
+    a[0] = 2;
+    a[1] = 1;
+    a[2] = 0;
     std::cout << a << std::endl;
+
+    list<int> b;
+    b.append(3);
+    b.append(2);
+    b.append(1);
+    std::cout << b << std::endl;
+
 }
