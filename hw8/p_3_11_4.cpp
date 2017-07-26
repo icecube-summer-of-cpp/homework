12bb8598e329a0c0f7f022017272ed0a7e0ccf2f
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-07-26 11:44:39 -0500

    Attempt at adding begin() and end(). NOT WORKING

--- hw8/p_3_11_4.cpp
+++ hw8/p_3_11_4.cpp
@@ -83,6 +83,110 @@ class vector
                 sum[i] = data[i] + that[i];
             return sum;
         }
+
+
+
+        struct iterator_entry
+        {
+            iterator_entry(const T& value)
+                : value(value), next(0) {}
+            T value;
+            iterator_entry* next;
+        };
+
+
+        class iterator
+        {
+            public:
+                using value_type = T;
+
+                iterator(iterator_entry* entry) : entry(entry) {}
+
+                iterator operator++() {entry = entry->next; return *this;}
+                iterator operator++(int) 
+                {
+                    iterator tmp(*this);
+                    entry = entry->next;
+                    return tmp;
+                }
+
+                T& operator*() {return entry->value;}
+                T* operator->() {return entry->value;}
+
+                bool operator==(const iterator& rhs)
+                {return entry==rhs.entry;}
+
+                bool operator!=(const iterator& rhs)
+                {
+                    std::cout << "neq: ";
+                    std::cout << entry << "!=" << rhs.entry << std::endl;
+                    return entry!=rhs.entry;}
+
+            private:
+                iterator_entry* entry;
+        };
+
+        class const_iterator
+        {
+            public:
+                using value_type = T;
+
+                const_iterator(iterator_entry* entry) : entry(entry) {}
+
+                const_iterator operator++() {entry = entry->next; return *this;}
+                const_iterator operator++(int) 
+                {
+                    const_iterator tmp(*this);
+                    entry = entry->next;
+                    return tmp;
+                }
+
+                const T& operator*() {return entry->value;}
+                const T* operator->() {return entry->value;}
+
+                bool operator==(const const_iterator& rhs)
+                {return entry->value == rhs->value;}
+
+                bool operator!=(const const_iterator& rhs)
+                {return entry->value != rhs->value;}
+
+            private:
+                iterator_entry* entry;
+        };
+
+
+        iterator begin()
+        {
+            std::cout << "begin" << std::endl;
+            if (my_size==0) {return iterator(0);}
+            std::cout << "  not zero" << std::endl;
+            iterator_entry curr(0);
+            std::cout << "  setup curr" << std::endl;
+            iterator_entry next(data[my_size-1]);
+            std::cout << "  setup next" << std::endl;
+            // std::cout << next.next;
+            // std::cout << "  final: " << next.next << std::endl;
+            for (int i=my_size-2; i>=0; --i)
+            {
+                std::cout << i << std::endl;
+                std::cout << "  next=" << next.value;
+                std::cout << " (" << next.next << ")" << std::endl;
+                curr.value = data[i];
+                std::cout << "  curr=" << curr.value;
+                curr.next = &next;
+                std::cout << "  curr.next=" << curr.next->value;
+                std::cout << " (" << curr.next << ")" << std::endl;
+                iterator_entry next = curr;
+            }
+            std::cout << "begin done" << std::endl;
+            return iterator(&curr);
+        }
+
+        iterator end()
+        {
+            std::cout << "end" << std::endl;
+            return iterator(0);
+        }
 };
 
 
@@ -99,9 +203,18 @@ std::ostream& operator<<(std::ostream& os, const vector<T>& v)
 
 int main()
 {
-    vector<int> a(3);
+    vector<int> a(5);
     a[0] = 2;
     a[1] = 1;
     a[2] = 0;
+    a[3] = 2;
+    a[4] = 4;
     std::cout << a << std::endl;
+
+    std::cout << "start loop" << std::endl;
+    for (auto b=a.begin(); b!=a.end(); b++)
+    {
+        std::cout << "loop" << std::endl;
+        std::cout << *b << std::endl;
+    }
 }
