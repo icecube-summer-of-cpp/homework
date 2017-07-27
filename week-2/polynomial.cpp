de5b6f1245a3f514227d545782562529c8208447
christophra <christoph@raab.io>
2017-07-27 17:14:27 +0200

    Exercise 2.8.2

--- week-2/polynomial.cpp
+++ week-2/polynomial.cpp
@@ -7,19 +7,29 @@
  * a destructor                                                       -> DONE ->
  * an output function for ostream.                                    -> DONE ->
  * (2.8.2)
- * Write a move assignment operator.
- * Define the copy constructor as default.
+ * Write a move assignment operator.                                  -> DONE ->
+ * Define the copy constructor as default.                            -> DONE ->
  * To test whether your assignment is used write a a function
  * polynomial f(double c2, double c1, double c0) 
- * that takes three coefficients and returns a polynomial. 
+ * that takes three coefficients and returns a polynomial.            -> DONE ->
  * Print out a message in your move assignment or use a debugger to make sure
- * your assignment is used.
+ * your assignment is used.                                           -> DONE ->   
  * (2.8.3)
- * Expand the program with a constructor and an assignment operator for an
- * initializer list. The degree of the polynomial should be the length of the
+ * Expand the program with
+ * a constructor and
+ * an assignment operator
+ * for an initializer list. The degree of the polynomial should be the length of the
  * initializer list minus one afterward.
  */
 Polynomial::Polynomial(const int& degree) : degree(degree), coeffs(std::vector<double>(degree+1, 0.)) {}
+
+Polynomial& Polynomial::operator=(Polynomial&& p)
+{
+    assert (degree == p.degree);
+    std::swap(coeffs,p.coeffs);
+    std::cout << "I like to move it move it" << std::endl;
+    return *this;
+}
 
 std::ostream& operator<<(std::ostream& os, const Polynomial& p)
 {
@@ -36,9 +46,27 @@ std::ostream& operator<<(std::ostream& os, const Polynomial& p)
 return os;
 }
 
+Polynomial parabola(double a0, double a1, double a2)
+{
+    Polynomial p(2);
+    p.coeffs[0] = a0;
+    p.coeffs[1] = a1;
+    p.coeffs[2] = a2;
+    return p;
+}
 
 int main()
 {
-    Polynomial p(2);
-    std::cout << p << std::endl;
+    Polynomial p1(2);
+    Polynomial p2(2);
+    std::cout << p1 << std::endl;
+    std::cout << p2 << std::endl;
+    std::cout<< "First assignment p2 = parabola(1.,2.0,0.5);" << std::endl;
+    p2 = parabola(1.,2.0,0.5);
+    std::cout << p1 << std::endl;
+    std::cout << p2 << std::endl;
+    std::cout<< "Second assignment p1 = std::move(p2);" << std::endl;
+    p1 = std::move(p2);
+    std::cout << p1 << std::endl;
+    std::cout << p2 << std::endl;
 }
--- week-2/polynomial.hpp
+++ week-2/polynomial.hpp
@@ -4,18 +4,26 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cassert>
+
 
 class Polynomial
 {   
+
+friend std::ostream& operator<< (std::ostream& stream, const Polynomial& p);
+//friend Polynomial& Polynomial::operator=(Polynomial&& p); // Making it a friend to read other p's data IS NOT NECESSARY
+friend Polynomial parabola(double a0, double a1, double a2);
+
 public:
     Polynomial(const int& degree);
     Polynomial(const Polynomial& p) = default; // Copy constructor
-    ~Polynomial() = default;
+    Polynomial& operator=(Polynomial&& p); // Move assignment.
+    ~Polynomial() = default; // int's and vectors can be destroyed and release their memory
     
-private:
-    friend std::ostream& operator<< (std::ostream& stream, const Polynomial& p);
+private:    
     int degree;
     std::vector<double> coeffs;
 };
 
+Polynomial parabola(double a0, double a1, double a2);
 std::ostream& operator<< (std::ostream& stream, const Polynomial &rhs);
