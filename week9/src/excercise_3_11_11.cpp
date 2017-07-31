0252dfb73f468cd58e8c609a716d25aac08578eb
René Reimann <renereimann@github.com>
2017-07-31 22:11:07 +0200

    solution for homework of week 9

--- week9/src/excercise_3_11_11.cpp
+++ week9/src/excercise_3_11_11.cpp
@@ -15,10 +15,50 @@
 //#include "gtest/gtest.h"
 #include <iostream>
 #include <memory>
+#include <cmath>
+
+template <typename F, typename T, unsigned N>
+class nth_derivative{
+    using prev_derivative = nth_derivative<F, T, N-1>;
+    public:
+        nth_derivative(const F& f, const T& h): h(h), fp(f, h) {}
+        T operator()(const T& x) const { return (fp(x+h) -fp(x)) /h; };
+        
+    private:
+        T               h;
+        prev_derivative fp;
+};
+
+template <typename F, typename T>
+class nth_derivative<F, T, 1>{
+    public:
+        nth_derivative(const F& f, const T& h): f(f), h(h) {}
+        T operator()(const T& x) const { return (f(x+h) -f(x)) /h; };
+        
+    private:
+        const F& f;
+        T        h;
+};
+
+template <unsigned N, typename F, typename T>
+nth_derivative<F, T, N>
+make_nth_derivative(const F& f, const T& h){ return nth_derivative<F, T, N>(f, h); }
+
+struct func_f{
+    double operator()(double x) const{
+        return 2*std::cos(x)+x*x;
+    };
+};
 
 int main (int argc, char **argv) 
 {
-    std::cout << "Test" << std::endl;
+    func_f func_o = func_f();
+    auto d_func_o = make_nth_derivative<1>(func_o, 0.001);
+    auto dd_func_o = make_nth_derivative<2>(func_o, 0.001);
+    std::cout << func_o(1.) << std::endl;
+    std::cout << d_func_o(1.) << std::endl;
+    std::cout << dd_func_o(1.) << std::endl;
+    
     //::testing::InitGoogleTest(&argc, argv);
     //return RUN_ALL_TESTS();
 };
--- week9/src/excercise_3_11_13.cpp
+++ week9/src/excercise_3_11_13.cpp
@@ -16,9 +16,19 @@
 #include <iostream>
 #include <memory>
 
+template <typename T, typename ...P>
+std::unique_ptr<T> make_unique(P ...p){ 
+    T* temp = new T(p...);
+    std::unique_ptr<T> ptr(temp);
+    return ptr;
+    };
 
 int main (int argc, char **argv) 
 {
+    int a = 3;
+    auto test = make_unique<int>(3);
+    test = make_unique<int>(7);
+    std::cout << *test << std::endl;
     std::cout << "Test" << std::endl;
     //::testing::InitGoogleTest(&argc, argv);
     //return RUN_ALL_TESTS();
