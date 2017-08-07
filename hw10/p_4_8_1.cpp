cc189dfa776f5e097815dce7fab457a3ffbaf5b4
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-08-07 12:59:56 -0500

    Fix problem number in title comment

--- hw10/p_4_8_1.cpp
+++ hw10/p_4_8_1.cpp
@@ -1,5 +1,5 @@
 /* Ben Hokanson-Fasig
- * Discovering Modern C++ - Exercise 3.11.10
+ * Discovering Modern C++ - Exercise 4.8.1
  * Create a vector of double and initialize it with the values
  * -9.3, -7.4, -3.8, -0.4, 1.3, 3.9, 5.4, 8.2. You can use an initializer list.
  * Sort the values by magnitude. Write
