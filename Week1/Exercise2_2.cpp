735371c05ed3f85250835398153dfc2058d71662
Ward <ward.vandriessche@ugent.be>
2017-08-15 12:11:40 +0200

    completed exercise 1.10.2.2

--- Week1/Exercise2_2.cpp
+++ Week1/Exercise2_2.cpp
@@ -1,8 +1,11 @@
+#include<iostream>
 
 int main(){
 	int array[10];
-	
-	int * parray=new int[10];
+	int newsize;
+	std::cout << "Size for array: ";
+	std::cin >> newsize;
+	int * parray=new int[newsize];
 
 	//delete[] parray;
 
