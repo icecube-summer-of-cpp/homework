117aa368ca3905ee70ccfd99be697bb94d2b66cd
kkrings <kai.krings88@gmx.net>
2017-08-17 21:51:32 +0200

    Finally add sim_particle and reco_particle to pokeball project.

--- kkrings/pokeball/main.cpp
+++ kkrings/pokeball/main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "particle.hpp"
@@ -22,4 +23,42 @@ int main()
 
     particle p3(particle_type::photon);
     std::cout << "Particle p3: id = " << p3.id() << std::endl;
+
+    sim_particle p4(particle_type::photon);
+    p4.set_shape(sim_particle::shape::dark);
+
+    std::string shape;
+    switch(p4.get_shape())
+    {
+        case sim_particle::shape::dark:
+            shape = "dark";
+            break;
+        case sim_particle::shape::cascade_segment:
+            shape = "cascade_segment";
+            break;
+        default:
+            shape = "unset";
+            break;
+    }
+
+    std::cout << "Particle p4: shape = " << shape << std::endl;
+
+    reco_particle p5(particle_type::photon);
+    p5.set_status(reco_particle::status::ok);
+
+    std::string status;
+    switch(p5.get_status())
+    {
+        case reco_particle::status::ok:
+            status = "ok";
+            break;
+        case reco_particle::status::failure:
+            status = "failure";
+            break;
+        default:
+            status = "unset";
+            break;
+    }
+
+    std::cout << "Particle p5: status = " << status << std::endl;
 }
--- kkrings/pokeball/particle.hpp
+++ kkrings/pokeball/particle.hpp
@@ -7,7 +7,6 @@
 
 /**
  * @brief Particle properties
- *
  * Define the properties of new particle types; supports non-integer
  * charges for defining non-Standard Model particles.
  */
@@ -18,7 +17,8 @@ struct properties
     unsigned int spin; /**< Particle spin times 2 */
 };
 
-enum class particle_type {
+enum class particle_type
+{
     unknown,
     photon,
     electron,
@@ -31,7 +31,6 @@ enum class particle_type {
 
 /**
  * @brief Particle properties read access
- *
  * This class gives read access to the properties of a particle object
  * of a certain type.
  */
@@ -39,7 +38,7 @@ class particle_properties
 {
     public:
         /**
-         * Construct properties of particle of a certain type.
+         * Construct properties of particle of type @p type.
          * @param type Particle type
          */
         particle_properties(particle_type type);
@@ -84,7 +83,6 @@ class particle_properties
 
 /**
  * @brief Particle class
- *
  * This class represents a free particle, holding its four-momentum and
  * four-position vectors, and its properties (type, mass, charge, spin).
  */
@@ -94,7 +92,7 @@ class particle
 
     public:
         /**
-         * Construct a particle of a certain type.
+         * Construct a particle of type @p type.
          * @param type Particle type
          */
         explicit particle(particle_type type) : properties_(type) {}
@@ -149,6 +147,111 @@ class particle
         vector4 momentum_;
         vector4 position_;
         particle_properties properties_;
+};
+
+/**
+ * @brief Simulated particle
+ */
+class sim_particle : public particle
+{
+    public:
+        enum class shape
+        {
+            unset,
+            dark,
+            cascade_segment
+        };
+
+        /**
+         * construct a particle of type @p type.
+         * @param type particle type
+         */
+        explicit sim_particle(particle_type type)
+            : particle(type), shape_(shape::unset)
+        {}
+
+        sim_particle() = default;
+        sim_particle(const sim_particle&) = default;
+        sim_particle(sim_particle&&) = default;
+
+        ~sim_particle() final = default;
+
+        sim_particle& operator=(const sim_particle&) = default;
+        sim_particle& operator=(sim_particle&&) = default;
+
+        /**
+         * @return particle's shape
+         */
+        shape get_shape() const
+        {
+            return shape_;
+        }
+
+        void set_shape(shape s) {shape_ = s;}
+
+    private:
+        shape shape_;
+};
+
+/**
+ * @brief Reconstructed particle
+ */
+class reco_particle : public particle
+{
+    public:
+        enum class shape
+        {
+            unset,
+            track,
+            cascade
+        };
+
+        enum class status
+        {
+            unset,
+            ok,
+            failure
+        };
+
+        /**
+         * Construct a particle of type @p type.
+         * @param type particle type
+         */
+        explicit reco_particle(particle_type type)
+            : particle(type), shape_(shape::unset), status_(status::unset)
+        {}
+
+        reco_particle() = default;
+        reco_particle(const reco_particle&) = default;
+        reco_particle(reco_particle&&) = default;
+
+        ~reco_particle() final = default;
+
+        reco_particle& operator=(const reco_particle&) = default;
+        reco_particle& operator=(reco_particle&&) = default;
+
+        /**
+         * @return Particle's shape
+         */
+        shape get_shape() const
+        {
+            return shape_;
+        }
+
+        /**
+         * @return Particle's fit status
+         */
+        status get_status() const
+        {
+            return status_;
+        }
+
+        void set_shape(shape s) {shape_ = s;}
+        void set_status(status s) {status_ = s;}
+
+    private:
+        shape shape_;
+        status status_;
 };
 
 #endif
